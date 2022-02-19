6388acfe58a8f03fa1a9ba2cd8f0ba4ea2e70599
Luis Riego <luisriego@users.noreply.github.com>
2022-02-19 09:57:58 -0300

    Update DoctrineUserRepository.php

--- app/src/Doctrine/DoctrineUserRepository.php
+++ app/src/Doctrine/DoctrineUserRepository.php
@@ -8,25 +8,25 @@ use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
-class DoctrineUserRepository implements UserRepository
+class DoctrineUserRepository // implements UserRepository
 {
-    private EntityRepository $repo;
-    private EntityManager $entityManager;
+//     private EntityRepository $repo;
+//     private EntityManager $entityManager;
 
-    public function __construct(ManagerRegistry $managerRegistry)
-    {
-        $this->repo = $managerRegistry->getRepository(User::class);
-        $this->entityManager = $managerRegistry->getManager();
-    }
+//     public function __construct(ManagerRegistry $managerRegistry)
+//     {
+//         $this->repo = $managerRegistry->getRepository(User::class);
+//         $this->entityManager = $managerRegistry->getManager();
+//     }
 
-    public function find(string $id): ?User
-    {
-        return $this->repo->find($id);
-    }
+//     public function find(string $id): ?User
+//     {
+//         return $this->repo->find($id);
+//     }
 
-    public function save(User $user): void
-    {
-        $this->entityManager->persist($user);
-        $this->entityManager->flush();
-    }
+//     public function save(User $user): void
+//     {
+//         $this->entityManager->persist($user);
+//         $this->entityManager->flush();
+//     }
 }
